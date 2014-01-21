67f2ada3039d0159ab81e024110e5195381e16a0
Arthur <arthur.4lves@gmail.com>
2014-01-21 11:37:31 -0200

    Corrige js

--- project/category/static/js/script.js
+++ project/category/static/js/script.js
@@ -1,4 +1,4 @@
 django.jQuery(document).ready(function () {
     $ = django.jQuery
-    $('.field-suggestion').find('p').html('<a href="javascript:;" class="addlink"> Funfou</a>');
+    $('.field-suggestion').find('p').append('<a href="javascript:;" class="addlink"> Funfou</a>');
 });
