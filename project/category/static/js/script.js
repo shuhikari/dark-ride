c9de0889f581d681320327679ffa6c1cb4429eee
Arthur <arthur.4lves@gmail.com>
2014-01-22 13:43:35 -0200

    Corrige a busca do chosen

--- project/category/static/js/script.js
+++ project/category/static/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 		no_results_text: "Sem resultados para",
 	    placeholder_text_single: "Selecione uma opção",
 	    placeholder_text_multiple: "Selecione as opções",
-	    width: '350px'.
+	    width: '350px',
 	    search_contains: true
 	});
 	$('.field-suggestion').on('click', 'a', function(){
