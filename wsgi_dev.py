2e6f13396c46ca0f6f48e2e0df5a269d4b075213
Édipo Maciel <shu.hikari@gmail.com>
2014-01-23 11:25:06 -0200

    Modifica wsgi_dev para ambiente de produção
    Necessário teste em produçao do endereço dos
    statics definidos no settings.

--- wsgi_dev.py
+++ wsgi_dev.py
@@ -1,3 +1,4 @@
+# !usr/bin/python
 """
 WSGI config for project project.
 
@@ -9,10 +10,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 import sys
-import site
 
 # Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('/home/intip/.virtualenvs/dark/lib/python2.6/site-packages')
+# asite.addsitedir('/home/intip/.virtualenvs/dark/lib/python2.6/site-packages')
 
 # Add the app's directory to the PYTHONPATH
 sys.path.append('/home/intip/ET/dark-ride/project/')
@@ -21,9 +21,6 @@ sys.path.append('/home/intip/ET/dark-ride/project/')
 activate_env = os.path.expanduser(
     "/home/intip/.virtualenvs/dark/bin/activate_this.py")
 execfile(activate_env, dict(__file__=activate_env))
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
