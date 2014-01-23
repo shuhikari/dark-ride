724db2fe7fee740ccccec0683cd824fb54a12739
Édipo Maciel <shu.hikari@gmail.com>
2014-01-23 11:47:34 -0200

    Corrige wsgi

--- wsgi_dev.py
+++ wsgi_dev.py
@@ -7,20 +7,21 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
 import os
 import sys
+# import site
 
 # Add the site-packages of the chosen virtualenv to work with
-# asite.addsitedir('/home/intip/.virtualenvs/dark/lib/python2.6/site-packages')
-
-# Add the app's directory to the PYTHONPATH
-sys.path.append('/home/intip/ET/dark-ride/project/')
+# site.addsitedir('/home/intip/.virtualenvs/dark/lib/python2.6/site-packages')
 
 # Activate your virtual env
 activate_env = os.path.expanduser(
     "/home/intip/.virtualenvs/dark/bin/activate_this.py")
 execfile(activate_env, dict(__file__=activate_env))
 
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/intip/ET/dark-ride/project/')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
